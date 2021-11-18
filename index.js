9de41552ad47850a4a0ed9326a9d99a3d2fe0ef1
Gloria Chan <changl3@oocl.com>
2021-11-18 14:09:23 +0800

    js-string Task

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
+console.log(" ' "+name.toUpperCase()+" ' ");
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
-var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
 
+//TODO write your code here...
+function UpperCase(str) {
+  var splitStr = str.toLowerCase().split(' ');
+  for (var i = 0; i < splitStr.length; i++) {
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+  }
+  return splitStr.join(' '); 
+}
+
+var sentence = 'good afternoon, mr mike.';
+console.log(UpperCase(sentence));
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+var moneynum = money.replace(/\D/g, "");
+console.log(moneynum);
